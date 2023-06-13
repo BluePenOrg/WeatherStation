4226f3041474c230af59e95deb7014bbea92d0b4
Gabriel Dias <gabrielmoreiradias270402@gmail.com>
2023-06-13 19:36:08 -0300

    Estacoes

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,20 +1,20 @@
-import { TextInput, TouchableOpacity, View, Text, Image} from "react-native";
+import { TextInput, TouchableOpacity, View, Text, Image } from "react-native";
 import { Logo } from '../assets';
 import { styles } from './styles/signStyles'
 
-export default function SignIn ({navigation}) {
+export default function SignIn({ navigation }) {
     return (
         <View style={styles.container}>
-            <Image source={Logo} style={{width: 200, height: 200}}/>
-            <TextInput placeholder="E-mail" style={styles.input}/>
-            <TextInput placeholder="Senha" style={styles.input}/>
-            <TextInput placeholder="Confirmar Senha" style={styles.input} secureTextEntry={true} autoCapitalize="none" autoCorrect={false} /> 
+            <Image source={Logo} style={{ width: 200, height: 200 }} />
+            <TextInput placeholder="E-mail" style={styles.input} />
+            <TextInput placeholder="Senha" style={styles.input} />
+            <TextInput placeholder="Confirmar Senha" style={styles.input} secureTextEntry={true} autoCapitalize="none" autoCorrect={false} />
             <TouchableOpacity style={styles.button} >
                 <Text style={styles.text}>Registrar-se</Text>
             </TouchableOpacity>
             <Text>
-                Já possui uma conta? <Text onPress={() => {navigation.navigate('SignIn')}} style={{color: 'blue', textDecorationLine: 'underline'}}>Entrar</Text>
-            </Text>        
+                Já possui uma conta? <Text onPress={() => { navigation.navigate('SignIn') }} style={{ color: 'blue', textDecorationLine: 'underline' }}>Entrar</Text>
+            </Text>
         </View>
     )
 }
--- mobile/src/screens/WeatherStations.tsx
+++ mobile/src/screens/WeatherStations.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
 
 export default function WeatherStations() {
+    const [location, setLocation] = useState({
+        latitude: -23.4698351198,
+        longitude: -47.4298077315
+      });
+      
     return (
-        <View style={styles.screen}>
-            <Text>Weather Stations</Text>
-        </View>
-    )
+    <View style={styles.screen}>
+        <MapView
+            style={{ width: '100%', height: '100%' }}
+            initialRegion={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }}
+            scrollEnabled={false}
+            >
+            <Marker coordinate={location} />
+        </MapView>
+    </View>
+);
+      
 }
 
 const styles = StyleSheet.create({
